c3a1e38c976f281f35be8b154ccfea4c90c1058b
Karl Gauck <kgauck@gmx.de>
2026-05-01 17:58:03 +0200

    calculating the orbit using fixed point method

--- src/assignments/assignment2.rs
+++ src/assignments/assignment2.rs
@@ -1,4 +1,5 @@
-
+use std::f64;
+use crate::utils;
 
 fn mean_anomaly(time: f64, phase: f64, period: f64) -> f64 {
     2. * std::f64::consts::PI * (time - phase) / period
@@ -8,36 +9,71 @@ fn fixed_point_iteration<T, F>(iterations: usize, starting_value: T, process: F)
     (0..iterations).fold(starting_value, |acc, i| process(&acc, i))
 }
 
-
-
 struct Orbit {
     eccentricity: f64,
     semimajor_axis: f64,
 }
 
-
-
-fn calculate_orbit(orbit: Orbit, points_on_orbit: usize, iterations: usize) {
+fn calculate_orbit_basic_fixpoint(orbit: &Orbit, points_on_orbit: usize, iterations: usize) -> Vec<f64> {
     let get_initial_value = |orbit: &Orbit, mean_anomaly: &f64| -> f64 {
-        if (orbit.eccentricity < 0.8) {
+        let ecc_threshold = 0.8;
+        if (orbit.eccentricity < ecc_threshold) {
             *mean_anomaly
         } else {
             std::f64::consts::PI
         }
     };
 
-    let mean_anomalies: Vec<f64> = (0..points_on_orbit).map(|x| ((x as f64)/(points_on_orbit as f64)) * 2. * std::f64::consts::PI).collect();
+    let mean_anomalies: Vec<f64> = (0..points_on_orbit).map(|x| ((x as f64)/(points_on_orbit as f64)) * 2. * f64::consts::PI).collect();
     let eccentric_anomalies = mean_anomalies.iter().map(|mean_anomaly|
         fixed_point_iteration(iterations, get_initial_value(&orbit, mean_anomaly), |old_eccentric_anomaly, _iterations| {
             mean_anomaly + orbit.eccentricity * f64::sin(*old_eccentric_anomaly)
         })
-    );
+    ).collect();
+
+    eccentric_anomalies
 }
 
 
+fn calculate_orbit_positions(orbit: &Orbit, eccentric_anomalies: Vec<f64>) -> Vec<(f64, f64)> {
 
+
+    let true_anomalies: Vec<f64> = eccentric_anomalies.iter().map(|ecc_anomaly| {
+        2.0*f64::atan2(f64::sqrt(1.+orbit.eccentricity) * f64::sin(*ecc_anomaly/2.0), f64::sqrt(1.-orbit.eccentricity) * f64::cos(*ecc_anomaly/2.0))
+    }).collect();
+
+
+    let sun_radii: Vec<f64> = true_anomalies.iter().map(|true_anomaly| {
+        orbit.semimajor_axis*(1.0 + orbit.eccentricity.powf(2.0)) / (1.0 + orbit.eccentricity * f64::cos(*true_anomaly))
+    }).collect();
+
+
+    let points: Vec<(f64, f64)> = sun_radii.iter().zip(true_anomalies).map(|(sun_radius, true_anomaly)| {
+        (sun_radius * f64::cos(true_anomaly), sun_radius * f64::sin(true_anomaly))
+    }).collect();
+
+    points
+}
 
 
 pub fn ex1() {
+    let orbits = vec!(
+        Orbit { eccentricity: 0.205, semimajor_axis: 0.39 },   // Mercury
+        Orbit { eccentricity: 0.967, semimajor_axis: 17.8 },   // Halley's comet
+    );
 
+    for orbit in orbits {
+        let iterations_counts = vec!(100, 3, 2, 1, 0);
+
+        let orbit_results: Vec<(Vec<(f64, f64)>, String)> = iterations_counts.iter().map(|iterations| {
+            let eccentric_anomalies = calculate_orbit_basic_fixpoint(&orbit, 256, *iterations as usize);
+            let points = calculate_orbit_positions(
+                &orbit, eccentric_anomalies);
+            (points, iterations.to_string())
+        }).collect();
+
+        utils::plotting::line_graph(
+            orbit_results
+        , false, "Orbit", "x in AU", "y in AU", "orbit.png")
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use crate::assignments::assignment1;
+use crate::assignments::assignment2;
 mod assignments;
 mod utils;
 
 fn main() {
     // assignment1::ex1("solutions/01/img");
-    assignment1::ex2();
+    // assignment1::ex2();
     // assignment1::ex3();
     // assignment1::ex4()
+    assignment2::ex1();
 
     println!("done");
 }
